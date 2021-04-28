8c98eee24fa77e77ceedab6be4a39a460635bd27
giuseppe <placidagiuseppe@gmail.com>
2021-04-28 16:35:57 +0200

    ....

--- index.php
+++ index.php
@@ -4,11 +4,14 @@ a schermo il paragrafo con la relative lunghezza e sostituire
 <?php
  $text="Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.";
  $badword = $_GET['nome'];
-
+ $title = "Benvenuto questo è il mio primo progetto in php";
+ $array = ["alfonzo","fabio","turuzzu"];
  ?>
- <p><?php echo $text ?></p>
+  <link rel="stylesheet" href="./master.css">
+  <h1><?php echo $title ?></h1>
+ <p id="blue"><?php echo $text ?></p>
 
  <div class="">
-  <p>  <?php echo strlen($text);?> </p>
-  <p> <?php  echo str_replace($badword, '***', $text);?>  </p>
+  <p id="green"> i caratteri del testo sono: <?php echo strlen($text);?> </p>
+  <p id="red"> <?php  echo str_replace($badword, '***', $text);?>  </p>
  </div>
